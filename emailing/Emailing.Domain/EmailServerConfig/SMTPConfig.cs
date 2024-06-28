bbb5fda14fa929eb586bea9bdb5eba2995f1958b
Giovani de Oliveira <giovani98@proton.me>
2024-06-28 12:24:43 -0300

    feat: Fetch smtp configuration parameter from environment variables

--- emailing/Emailing.Domain/EmailServerConfig/SMTPConfig.cs
+++ emailing/Emailing.Domain/EmailServerConfig/SMTPConfig.cs
@@ -7,21 +7,29 @@ namespace Emailing.Domain.EmailServerConfig;
 public class SMTPConfig : ISMPTConfig
 {
     private string _smtpServer;
-    private int _port;
+    private string _port;
     private string _senderEmail;
     private string _senderPassword;
 
-    public SMTPConfig(IConfiguration configuration)
+    // É necessário configurar as variáveis de ambiente para enviar emails
+    // Lembre-se de reiniciar o Visual Studio após configurar as variáveis de ambiente.
+    /*
+     * Eu dou preferência para o Gmail, então o smtp server deve ser da Google
+     * A porta usada é TLS
+     * No sender email, dê preferência a um Gmail
+     * Em sender password, é necessário gerar uma senha de aplicativo em sua conta, ao invés de usar sua senha de acesso
+     */
+    public SMTPConfig()
     {
-        _smtpServer = "smtp.gmail.com";
-        _port = 587;
-        _senderEmail = "";
-        _senderPassword = "";
+        _smtpServer = Environment.GetEnvironmentVariable("ORANGE_TASKS_SMTP_SERVER")!;
+        _port = Environment.GetEnvironmentVariable("ORANGE_TASKS_SMTP_PORT")!;
+        _senderEmail = Environment.GetEnvironmentVariable("ORANGE_TASKS_SENDER_EMAIL")!;
+        _senderPassword = Environment.GetEnvironmentVariable("ORANGE_TASKS_SENDER_APP_PASSWORD")!;
     }
 
     public async Task SendEmailAsync(string recipient, string subject, string body)
     {
-        var client = new SmtpClient(_smtpServer, _port)
+        var client = new SmtpClient(_smtpServer, int.Parse(_port))
         {
             EnableSsl = true,
             UseDefaultCredentials = false,
