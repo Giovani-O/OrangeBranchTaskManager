53e0d594894afe5b070149913150c462cb08bc34
Giovani <giovani98@proton.me>
2024-07-17 14:43:49 -0300

    feat: Finish token unit tests

--- tests/OrangeBranchTaskManager.Application.Tests/UseCasesTests/TokenService/TokenServiceUnitTests.cs
+++ tests/OrangeBranchTaskManager.Application.Tests/UseCasesTests/TokenService/TokenServiceUnitTests.cs
@@ -6,37 +6,44 @@ using Microsoft.Extensions.Configuration;
 using Moq;
 using OrangeBranchTaskManager.Application.Tests.TestUtilities.Tools;
 using OrangeBranchTaskManager.Application.UseCases.Token.TokenService;
+using OrangeBranchTaskManager.Exception;
+using OrangeBranchTaskManager.Exception.ExceptionsBase;
 
 namespace OrangeBranchTaskManager.Application.Tests.UseCasesTests.TokenService;
 
 public class TokenServiceUnitTests
 {
     private readonly Mock<IConfiguration> _configurationMock;
+    private readonly Mock<IConfigurationSection> _keySectionMock;
+    private readonly Mock<IConfigurationSection> _tokenValidityInMinutesSectionMock;
+    private readonly Mock<IConfigurationSection> _validAudienceSectionMock;
+    private readonly Mock<IConfigurationSection> _validIssuerSectionMock;
+    private readonly Mock<IConfigurationSection> _jwtSectionMock;
 
     public TokenServiceUnitTests()
     {
         // Inicia mocks
         _configurationMock = new Mock<IConfiguration>();
-        Mock<IConfigurationSection> jwtSectionMock = new();
-        Mock<IConfigurationSection> keySectionMock = new();
-        Mock<IConfigurationSection> tokenValidityInMinutesSectionMock = new();
-        Mock<IConfigurationSection> validAudienceSectionMock = new();
-        Mock<IConfigurationSection> validIssuerSectionMock = new();
+        _keySectionMock = new Mock<IConfigurationSection>(); 
+        _tokenValidityInMinutesSectionMock = new Mock<IConfigurationSection>();
+        _validAudienceSectionMock = new Mock<IConfigurationSection>();
+        _validIssuerSectionMock = new Mock<IConfigurationSection>();
+        _jwtSectionMock = new Mock<IConfigurationSection>();
 
         // Atribui valores válidos
-        keySectionMock.Setup(config => config.Value).Returns(StringGenerator.NewString(32));
-        tokenValidityInMinutesSectionMock.Setup(config => config.Value).Returns("60");
-        validAudienceSectionMock.Setup(config => config.Value).Returns("AudienceMock");
-        validIssuerSectionMock.Setup(config => config.Value).Returns("IssuerMock");
+        _keySectionMock.Setup(config => config.Value).Returns(StringGenerator.NewString(32));
+        _tokenValidityInMinutesSectionMock.Setup(config => config.Value).Returns("60");
+        _validAudienceSectionMock.Setup(config => config.Value).Returns("AudienceMock");
+        _validIssuerSectionMock.Setup(config => config.Value).Returns("IssuerMock");
         
         // Configura GetSection
-        jwtSectionMock.Setup(config => config.GetSection("Key")).Returns(keySectionMock.Object);
-        jwtSectionMock.Setup(config => config.GetSection("TokenValidityInMinutes"))
-            .Returns(tokenValidityInMinutesSectionMock.Object);
-        jwtSectionMock.Setup(config => config.GetSection("ValidAudience")).Returns(validAudienceSectionMock.Object);
-        jwtSectionMock.Setup(config => config.GetSection("ValidIssuer")).Returns(validIssuerSectionMock.Object);
+        _jwtSectionMock.Setup(config => config.GetSection("Key")).Returns(_keySectionMock.Object);
+        _jwtSectionMock.Setup(config => config.GetSection("TokenValidityInMinutes"))
+            .Returns(_tokenValidityInMinutesSectionMock.Object);
+        _jwtSectionMock.Setup(config => config.GetSection("ValidAudience")).Returns(_validAudienceSectionMock.Object);
+        _jwtSectionMock.Setup(config => config.GetSection("ValidIssuer")).Returns(_validIssuerSectionMock.Object);
         
-        _configurationMock.Setup(config => config.GetSection("JWT")).Returns(jwtSectionMock.Object);
+        _configurationMock.Setup(config => config.GetSection("JWT")).Returns(_jwtSectionMock.Object);
     }
     
     [Fact]
@@ -56,5 +63,28 @@ public class TokenServiceUnitTests
         
         result.Should().NotBeNull();
         result.Should().BeOfType<JwtSecurityToken>();
+    }
+
+    [Fact]
+    public void Should_Throw_Exception_On_Invalid_Secret_Key()
+    { 
+        _keySectionMock.Setup(config => config.Value).Returns((string)null!);
+        
+        var useCase = new TokenServiceUseCase();
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, "TestUser"),
+            new Claim(ClaimTypes.Email, "test@test.com"),
+            new Claim("id", "TestUser"),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        Action result = () => useCase.Execute(claims, _configurationMock.Object);
+
+        var exception = result.Should().Throw<ErrorOnExecutionException>();
+        var errors = exception.Which.GetErrors();
+        
+        errors.Should().ContainKey(ResourceErrorMessages.ERROR)
+            .WhoseValue.Should().Contain(ResourceErrorMessages.ERROR_INVALID_SECRET_KEY);
     }
 }
